ed6000eb6802750ac4a9078bfdc887720350f6ee
Alex <57911465+AlexKrayVR@users.noreply.github.com>
2021-02-15 22:11:53 +0300

    order by ID complete

--- app/src/main/java/yelm/io/yelm/order/user_order/OrderByIDActivity.java
+++ app/src/main/java/yelm/io/yelm/order/user_order/OrderByIDActivity.java
@@ -1,13 +1,24 @@
 package yelm.io.yelm.order.user_order;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.room.FtsOptions;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.appbar.AppBarLayout;
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraListener;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CameraUpdateSource;
+import com.yandex.mapkit.map.Map;
+
 
 import org.jetbrains.annotations.NotNull;
 
@@ -38,6 +49,7 @@ import yelm.io.yelm.support_stuff.Logging;
 public class OrderByIDActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
     ActivityOrderByIDBinding binding;
     OrderProductAdapter orderProductAdapter;
+    private static final float DEFAULT_ZOOM = 16f;
 
     private int maxScrollSize;
     private static final int PERCENTAGE_TO_SHOW_IMAGE = 80;
@@ -46,19 +58,24 @@ public class OrderByIDActivity extends AppCompatActivity implements AppBarLayout
     SimpleDateFormat currentFormatterDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     SimpleDateFormat printedFormatterDate = new SimpleDateFormat("yyyy-MM-dd");
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        MapKitFactory.setApiKey(getString(R.string.yandex_maps_API_key));
+        MapKitFactory.initialize(this);
         super.onCreate(savedInstanceState);
         binding = ActivityOrderByIDBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         binding.back.setOnClickListener(v -> finish());
+        binding.mapView.getMap().setScrollGesturesEnabled(false);
+        binding.mapView.getMap().setZoomGesturesEnabled(false);
+        binding.mapView.getMap().setFastTapEnabled(false);
 
         Bundle args = getIntent().getExtras();
         if (args != null) {
             Log.d(Logging.debug, "Method onCreate() - id: " + args.getString("id"));
             getOrder(args.getString("id"));
         }
-
     }
 
     private void getOrder(String id) {
@@ -76,6 +93,15 @@ public class OrderByIDActivity extends AppCompatActivity implements AppBarLayout
                         if (response.isSuccessful()) {
                             Log.d(Logging.debug, "isSuccessful");
                             if (response.body() != null) {
+
+                                binding.mapView.getMap().move(
+                                        new CameraPosition(new Point(Double.parseDouble(response.body().getLatitude()),
+                                                Double.parseDouble(response.body().getLongitude())),
+                                                DEFAULT_ZOOM,
+                                                0.0f,
+                                                0.0f),
+                                        new Animation(Animation.Type.LINEAR, 0),
+                                        null);
                                 Calendar date = GregorianCalendar.getInstance();
                                 try {
                                     date.setTime(Objects.requireNonNull(currentFormatterDate.parse(response.body().getCreatedAt())));
@@ -92,7 +118,20 @@ public class OrderByIDActivity extends AppCompatActivity implements AppBarLayout
                                         printedFormatterDate.format(date.getTime()),
                                         getText(R.string.orderByIDActivityOrderAddress),
                                         response.body().getAddress()));
+
+
                                 binding.collapsingToolbar.setTitle(response.body().getTransactionStatus());
+
+                                if (response.body().getTransactionStatus().equals("Подтвержденный")) {
+                                    binding.collapsingToolbar.setCollapsedTitleTextColor(getResources().getColor(R.color.greenColor));
+                                    binding.collapsingToolbar.setExpandedTitleColor(getResources().getColor(R.color.greenColor));
+                                }else if (response.body().getTransactionStatus().equals("Неподтвержденный")){
+                                    binding.collapsingToolbar.setCollapsedTitleTextColor(getResources().getColor(R.color.colorOrangeText));
+                                    binding.collapsingToolbar.setExpandedTitleColor(getResources().getColor(R.color.colorOrangeText));
+                                } else if (response.body().getTransactionStatus().equals("Отмененный")){
+                                    binding.collapsingToolbar.setCollapsedTitleTextColor(getResources().getColor(R.color.redColor));
+                                    binding.collapsingToolbar.setExpandedTitleColor(getResources().getColor(R.color.redColor));
+                                }
 
                                 orderProductAdapter = new OrderProductAdapter(OrderByIDActivity.this,
                                         response.body().getItemsInfo(),
@@ -143,5 +182,19 @@ public class OrderByIDActivity extends AppCompatActivity implements AppBarLayout
         }
     }
 
+    @Override
+    protected void onStop() {
+        binding.mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        binding.mapView.onStart();
+
+    }
 
 }
